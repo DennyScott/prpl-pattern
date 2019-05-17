30f5ee3f8c124f4f3b337d9ad42515f5b9336c32
Denny Scott <dennyscott301@gmail.com>
2019-05-17 11:31:21 -0700

    Add react lazy loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
+import Loadable from 'react-loadable';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import ImageList from "./features/image-list";
-import About from "./features/about";
 import Navigation from './features/navigation';
+
+import Loading from './components/loading';
 
 import { PicSumContext } from "./context/picsum-context";
 import { useFetch } from "./utils/useFetch";
 import endpoints from "./utils/endpoints";
+
+const ImageListPage = Loadable({
+  loader: () => import('./features/image-list'),
+  loading: Loading
+});
+
+const AboutPage = Loadable({
+  loader: () => import('./features/about'),
+  loading: Loading
+});
 
 function App() {
   const [ picSum, setPicSum ] = useState([]); 
@@ -17,8 +28,8 @@ function App() {
       <Router>
         <div>
           <Route path="/" component={Navigation} />
-          <Route path="/images" exact component={ImageList} />
-          <Route path="/about" exact component={About} />
+          <Route path="/images" exact component={ImageListPage} />
+          <Route path="/about" exact component={AboutPage} />
         </div>
       </Router>
     </PicSumContext.Provider>
